b973280510f147f08329d3409fff7f26d9e2e145
nguyenminhthuan2309 <thuannguyen2392@gmail.com>
2025-03-19 19:07:57 +0700

    update user update dto

--- readingbookonline_be/src/features/user/dto/update-user.dto.ts
+++ readingbookonline_be/src/features/user/dto/update-user.dto.ts
@@ -1,7 +1,13 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty, PartialType, PickType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
-import { PickType } from '@nestjs/swagger';
 
 export class UpdateUserDto extends PartialType(
   PickType(CreateUserDto, ['name'] as const),
-) {}
+) {
+  @ApiProperty({
+    description: 'Tên mới của người dùng',
+    example: 'Mạnh1234',
+    required: false,
+  })
+  name?: string;
+}
