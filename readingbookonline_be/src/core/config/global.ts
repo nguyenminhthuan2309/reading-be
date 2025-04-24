f765630541fe73aa991b03d80241fa98066504b5
nguyenminhthuan2309 <thuannguyen2392@gmail.com>
2025-04-24 10:07:50 +0700

    add guard

--- readingbookonline_be/src/core/config/global.ts
+++ readingbookonline_be/src/core/config/global.ts
@@ -5,9 +5,9 @@ import { DataSourceOptions } from 'typeorm';
 config({ path: path.join(process.cwd(), 'src', '.env') });
 export const redisConfig = {
   host: process.env.REDIS_HOST || 'localhost',
-  port: Number(process.env.REDIS_PORT) || 41379,
-  username: process.env.REDIS_USERNAME || 'dev',
-  password: process.env.REDIS_PASSWORD || 'ZGV2QGVsYWN0aWM=',
+  port: Number(process.env.REDIS_PORT) || 6379,
+  username: process.env.REDIS_USERNAME || 'default',
+  password: process.env.REDIS_PASSWORD || '1234',
 };
 
 export const postgresConfig: DataSourceOptions = {
